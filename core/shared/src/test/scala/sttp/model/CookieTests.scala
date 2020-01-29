c0adcffb6c5fef1c1374f53296cfc319a4e1aead
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2020-01-29 20:14:31 +0530

    added test for toString method for CookieWithMeta class

--- core/shared/src/test/scala/sttp/model/CookieTests.scala
+++ core/shared/src/test/scala/sttp/model/CookieTests.scala
@@ -7,7 +7,7 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class CookieTests extends AnyFunSuite with Matchers with TryValues {
-  test("should parse a cookie with unknown attributes") {
+  test("parse - should parse a cookie with unknown attributes") {
     val cookieRawValue =
       "WELCOME_STATE_CHECKER=Xh3JYGEWrY7X2CIyI8_xqJ4UkipYtamLS2YOU27h8vY; Version=1; Something-Else; expires=Tue, 28-Jan-2020 17:13:10 GMT; max-Age=300; Path=/; HttpOnly"
 
@@ -31,5 +31,14 @@ class CookieTests extends AnyFunSuite with Matchers with TryValues {
         )
       )
     )
+  }
+
+  test("toString - should get string representation of cookie with all its attributes") {
+    val cookieRawValue =
+      "WELCOME_STATE_CHECKER=Xh3JYGEWrY7X2CIyI8_xqJ4UkipYtamLS2YOU27h8vY; Version=1; Something-Else; expires=Tue, 28-Jan-2020 17:13:10 GMT; max-Age=300; Path=/; HttpOnly"
+
+    val result = CookieWithMeta.parse(cookieRawValue)
+
+    result.right.get.toString shouldBe "WELCOME_STATE_CHECKER=Xh3JYGEWrY7X2CIyI8_xqJ4UkipYtamLS2YOU27h8vY; Expires=Tue, 28 Jan 2020 17:13:10 GMT; Max-Age=300; Path=/; HttpOnly; Version=1; Something-Else"
   }
 }
