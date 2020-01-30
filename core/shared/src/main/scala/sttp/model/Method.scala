67e5deb4344ff13e263eb2d9c2d01a412eb2e5f1
adamw <adam@warski.org>
2020-01-30 19:03:00 +0100

    Add methods to check if a method is safe/idempotent

--- core/shared/src/main/scala/sttp/model/Method.scala
+++ core/shared/src/main/scala/sttp/model/Method.scala
@@ -16,6 +16,23 @@ object Method extends Methods {
   def safeApply(method: String): Either[String, Method] =
     Validate.all(validateToken("Method", method))(notValidated(method))
   def notValidated(method: String) = new Method(method)
+
+  /**
+    * An HTTP method is idempotent if an identical request can be made once or several times in a row with the same
+    * effect while leaving the server in the same state.
+    * @see https://developer.mozilla.org/en-US/docs/Glossary/Idempotent
+    */
+  def isIdempotent(m: Method): Boolean = {
+    m == Method.HEAD || m == Method.TRACE || m == Method.GET || m == Method.PUT || m == Method.OPTIONS || m == Method.DELETE
+  }
+
+  /**
+    * An HTTP method is safe if it doesn't alter the state of the server.
+    * @see https://developer.mozilla.org/en-US/docs/Glossary/safe
+    */
+  def isSafe(m: Method): Boolean = {
+    m == Method.HEAD || m == Method.GET || m == Method.OPTIONS
+  }
 }
 
 trait Methods {
