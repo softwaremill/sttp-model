37557e0ea1d5d8ce8632cbf1b66e66b29ffafdd9
adamw <adam@warski.org>
2020-08-19 15:30:01 +0200

    Remove files from the shared project

--- ws/src/main/scala/sttp/ws/WebSocketException.scala
+++
@@ -1,7 +0,0 @@
-package sttp.ws
-
-abstract class WebSocketException(msg: String) extends Exception(msg)
-
-class WebSocketClosed() extends WebSocketException(null)
-
-class WebSocketBufferFull(capacity: Int) extends WebSocketException(s"Buffered $capacity messages")
