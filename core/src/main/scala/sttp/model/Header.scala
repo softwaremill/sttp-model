796326627cd48b47f52b98829bf07fafb0e84f15
adamw <adam@warski.org>
2021-01-04 12:30:32 +0100

    Caching headers

--- core/src/main/scala/sttp/model/Header.scala
+++ core/src/main/scala/sttp/model/Header.scala
@@ -5,6 +5,9 @@ import sttp.model.internal.Validate
 import sttp.model.internal.Rfc2616.validateToken
 import sttp.model.internal.Validate._
 
+import java.time.{Instant, ZoneId}
+import java.time.format.DateTimeFormatter
+import scala.concurrent.duration.FiniteDuration
 import scala.util.hashing.MurmurHash3
 
 /** An HTTP header. The [[name]] property is case-insensitive during equality checks.
@@ -80,12 +83,55 @@ object Header {
     Header(HeaderNames.AccessControlRequestMethod, method.toString)
   def authorization(authType: String, credentials: String): Header =
     Header(HeaderNames.Authorization, s"$authType $credentials")
+  def cacheControl(
+      maxAge: Option[FiniteDuration] = None,
+      maxStale: Option[Option[FiniteDuration]] = None,
+      minFresh: Option[FiniteDuration] = None,
+      noCache: Boolean = false,
+      noStore: Boolean = false,
+      noTransform: Boolean = false,
+      onlyIfCached: Boolean = false,
+      mustRevalidate: Boolean = false,
+      public: Boolean = false,
+      `private`: Boolean = false,
+      proxyRevalidate: Boolean = false,
+      sMaxage: Option[FiniteDuration] = None,
+      immutable: Boolean = false,
+      staleWhileRevalidate: Option[FiniteDuration] = None,
+      staleIfError: Option[FiniteDuration] = None
+  ): Header = {
+    val values = scala.collection.mutable.SortedSet[String]()
+    maxAge.foreach(v => values.add(s"max-age=${v.toSeconds}"))
+    maxStale.foreach {
+      case Some(v) => values.add(s"max-stale=${v.toSeconds}")
+      case None    => values.add("max-stale")
+    }
+    minFresh.foreach(v => values.add(s"min-fresh=${v.toSeconds}"))
+    if (noCache) values.add(s"no-cache")
+    if (noStore) values.add(s"no-store")
+    if (noTransform) values.add(s"no-transform")
+    if (onlyIfCached) values.add(s"only-if-cached")
+    if (mustRevalidate) values.add(s"must-revalidate")
+    if (public) values.add(s"public")
+    if (`private`) values.add(s"private")
+    if (proxyRevalidate) values.add(s"proxy-revalidate")
+    sMaxage.foreach(v => values.add(s"s-maxage=${v.toSeconds}"))
+    if (immutable) values.add(s"immutable")
+    staleWhileRevalidate.foreach(v => values.add(s"stale-while-revalidate=${v.toSeconds}"))
+    staleIfError.foreach(v => values.add(s"stale-if-error=${v.toSeconds}"))
+    Header(HeaderNames.CacheControl, values.mkString(", "))
+  }
   def contentLength(length: Long): Header = Header(HeaderNames.ContentLength, length.toString)
   def contentEncoding(encoding: String): Header = Header(HeaderNames.ContentEncoding, encoding)
   def contentType(mediaType: MediaType): Header = Header(HeaderNames.ContentType, mediaType.toString)
   def cookie(firstCookie: Cookie, otherCookies: Cookie*): Header =
     Header(HeaderNames.Cookie, (firstCookie +: otherCookies).map(_.toString).mkString("; "))
-  def etag(tag: String): Header = Header(HeaderNames.Etag, tag)
+  def etag(tag: String, weak: Boolean = false): Header =
+    Header(HeaderNames.Etag, s"""${if (weak) "W/" else ""}"$tag"""")
+  def expires(i: Instant): Header =
+    Header(HeaderNames.Expires, DateTimeFormatter.RFC_1123_DATE_TIME.format(i.atZone(GMT)))
+  def lastModified(i: Instant): Header =
+    Header(HeaderNames.LastModified, DateTimeFormatter.RFC_1123_DATE_TIME.format(i.atZone(GMT)))
   def location(uri: String): Header = Header(HeaderNames.Location, uri)
   def location(uri: Uri): Header = Header(HeaderNames.Location, uri.toString)
   def proxyAuthorization(authType: String, credentials: String): Header =
@@ -94,4 +140,6 @@ object Header {
   def userAgent(userAgent: String): Header = Header(HeaderNames.UserAgent, userAgent)
   def xForwardedFor(firstAddress: String, otherAddresses: String*): Header =
     Header(HeaderNames.XForwardedFor, (firstAddress +: otherAddresses).mkString(", "))
+
+  private val GMT = ZoneId.of("GMT")
 }
--- core/src/test/scalajvm/sttp/model/HeaderTests.scala
+++ core/src/test/scalajvm/sttp/model/HeaderTests.scala
@@ -3,6 +3,10 @@ package sttp.model
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import scala.concurrent.duration.DurationInt
+
 class HeaderTests extends AnyFlatSpec with Matchers {
   it should "return a string description of the header" in {
     Header.unsafeApply(HeaderNames.Authorization, "xyz").toString shouldBe "Authorization: xyz"
@@ -25,5 +29,28 @@ class HeaderTests extends AnyFlatSpec with Matchers {
     val h = Header.authorization("bearer", "123")
     h.toString shouldBe "Authorization: bearer 123"
     h.toStringSafe() shouldBe "Authorization: ***"
+  }
+
+  it should "properly create a cache-control header" in {
+    Header.cacheControl(maxAge = Some(3.seconds)).toString shouldBe "Cache-Control: max-age=3"
+    Header.cacheControl(noCache = true).toString shouldBe "Cache-Control: no-cache"
+    Header.cacheControl(maxStale = Some(None)).toString shouldBe "Cache-Control: max-stale"
+    Header.cacheControl(maxStale = Some(Some(1.second))).toString shouldBe "Cache-Control: max-stale=1"
+    Header
+      .cacheControl(noTransform = true, public = true, sMaxage = Some(10.seconds))
+      .toString shouldBe "Cache-Control: no-transform, public, s-maxage=10"
+  }
+
+  it should "properly quote an etag" in {
+    Header.etag("xyz").toString shouldBe "ETag: \"xyz\""
+  }
+
+  it should "properly quote a weak etag" in {
+    Header.etag("xyz", weak = true).toString shouldBe "ETag: W/\"xyz\""
+  }
+
+  it should "properly format expires date" in {
+    val i = ZonedDateTime.parse("Wed, 21 Oct 2015 07:28:00 GMT", DateTimeFormatter.RFC_1123_DATE_TIME)
+    Header.expires(i.toInstant).toString shouldBe "Expires: Wed, 21 Oct 2015 07:28:00 GMT"
   }
 }
