c8ababd18a3dd578ab6efd16db03c2a4c1dedc25
ghostbuster91 <kghost0@gmail.com>
2021-05-04 10:11:35 +0200

    Method should not be case sensitive

---
+++ core/src/test/scala/sttp/model/MethodTest.scala
@@ -0,0 +1,18 @@
+package sttp.model
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class MethodTest extends AnyFlatSpec with Matchers {
+  it should "method should not be case sensitive" in {
+    Method.unsafeApply("get") shouldBe Method.unsafeApply("GET")
+    Method.unsafeApply("head") shouldBe Method.unsafeApply("HEAD")
+    Method.unsafeApply("post") shouldBe Method.unsafeApply("POST")
+    Method.unsafeApply("put") shouldBe Method.unsafeApply("PUT")
+    Method.unsafeApply("delete") shouldBe Method.unsafeApply("DELETE")
+    Method.unsafeApply("options") shouldBe Method.unsafeApply("OPTIONS")
+    Method.unsafeApply("patch") shouldBe Method.unsafeApply("PATCH")
+    Method.unsafeApply("connect") shouldBe Method.unsafeApply("CONNECT")
+    Method.unsafeApply("trace") shouldBe Method.unsafeApply("TRACE")
+  }
+}
