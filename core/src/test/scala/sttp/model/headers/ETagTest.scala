33ab1ef630e73cd4830cef6ae2f9f1ac0f385e27
adamw <adam@warski.org>
2021-08-13 09:27:11 +0200

    Parse/serialize lists of etags

--- core/src/test/scala/sttp/model/headers/ETagTest.scala
+++ core/src/test/scala/sttp/model/headers/ETagTest.scala
@@ -8,12 +8,26 @@ class ETagTest extends AnyFlatSpec with Matchers {
     ETag("xyz").toString shouldBe "\"xyz\""
   }
 
+  it should "properly quote a list of etags" in {
+    ETag.toString(List(ETag("xyz"), ETag("abc"), ETag("123"))) shouldBe "\"xyz\", \"abc\", \"123\""
+  }
+
   it should "properly quote a weak etag" in {
     ETag("xyz", weak = true).toString shouldBe "W/\"xyz\""
   }
 
   it should "parse an etag" in {
     ETag.parse("\"xyz\"") shouldBe Right(ETag("xyz"))
+  }
+
+  it should "parse a list of etags" in {
+    ETag.parseList("\"xyz\", \"abc\", \"123\"") shouldBe Right(List(ETag("xyz"), ETag("abc"), ETag("123")))
+  }
+
+  it should "parse a list of etags with weak etags" in {
+    ETag.parseList("\"xyz\", W/\"abc\", \"123\"") shouldBe Right(
+      List(ETag("xyz"), ETag("abc", weak = true), ETag("123"))
+    )
   }
 
   it should "parse a weak etag" in {
