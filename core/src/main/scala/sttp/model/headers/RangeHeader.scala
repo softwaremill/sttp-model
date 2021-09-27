badbcc8db0295f6597aedc802ddce2a1e6e65b60
bartekzylinski <bartlomiej.zylinski@softwaremill.pl>
2021-09-27 08:11:03 +0200

    Fixed RangeHeader to cover remaining cases

--- core/src/main/scala/sttp/model/headers/RangeHeader.scala
+++ core/src/main/scala/sttp/model/headers/RangeHeader.scala
@@ -5,7 +5,7 @@ import sttp.model.{ContentRangeUnits, HeaderNames}
 
 import scala.util.Try
 
-case class RangeHeader(start: Int, end: Int, unit: String) {
+case class RangeHeader(start: Option[Int], end: Option[Int], unit: String) {
 
   val range: Range = Range(start, end)
 
@@ -14,32 +14,45 @@ case class RangeHeader(start: Int, end: Int, unit: String) {
 
 object RangeHeader {
 
-  def parse(str: String): Either[String, RangeHeader] = {
+  def parse(str: String): Either[String, List[RangeHeader]] = {
     Try(parseString(str))
-      .filter(r => isValid(r.start, r.end))
+      .filter(_.forall(r => isValid(r.start, r.end)))
       .toEither.left
       .map(_.getMessage)
   }
 
-  private def parseString(str: String): RangeHeader = {
+  private def parseString(str: String): List[RangeHeader] = {
     val splited = str.split("=")
     val unit = splited(0)
-    val range = splited(1).split("-")
-    // TODO add support for other cases of range
-    RangeHeader(range(0).toInt, range(1).toInt, unit)
+    if (splited(1).contains(",")) splited(1).split(",").map(s => parsSingleRange(s, unit)).toList
+    else List(parsSingleRange(splited(1), unit))
   }
 
-  private def isValid(start: Int, end: Int): Boolean = start < end
+  private def parsSingleRange(rangeString: String, unit: String): RangeHeader = {
+    val strings = rangeString.split("-")
+    val range = if (strings.size == 2)
+      RangeHeader(strings(0).toIntOption, strings(1).toIntOption, unit)
+    else RangeHeader(strings(0).toIntOption, None, unit)
+    range
+  }
 
-  def unsafeApply(s: String): RangeHeader = safeApply(s).getOrThrow
+  private def isValid(start: Option[Int], end: Option[Int]): Boolean = {
+    val isCorrectlyDefined = start.isDefined || end.isDefined
+    if (start.isDefined && end.isDefined) {
+      val isRangeValid = start.zip(end).exists(startToEnd => startToEnd._1 < startToEnd._2)
+      isCorrectlyDefined && isRangeValid
+    } else isCorrectlyDefined
+  }
 
-  def safeApply(s: String): Either[String, RangeHeader] = parse(s)
+  def unsafeApply(s: String): List[RangeHeader] = safeApply(s).getOrThrow
+
+  def safeApply(s: String): Either[String, List[RangeHeader]] = parse(s)
 }
 
-case class Range(start: Int, end: Int) {
-  def isValid(contentLength: Long): Boolean = if (end < contentLength) true else false
+case class Range(start: Option[Int], end: Option[Int]) {
+  def isValid(contentLength: Long): Boolean = if (end.exists(e => e < contentLength)) true else false
 
-  def toContentRange(fileSize: Long, unit: String = ContentRangeUnits.Bytes): String = unit + " " + start + "-" + end + "/" + fileSize
+  def toContentRange(fileSize: Long, unit: String = ContentRangeUnits.Bytes): String = unit + " " + start.getOrElse(0) + "-" + end.getOrElse(0) + "/" + fileSize
 
-  val contentLength: Int = end - start
+  val contentLength: Int = end.zip(start).map(r => r._1 - r._2).getOrElse(0)
 }
