0bb057a38ad255b9440dbea1a1ddd5a9795cdde1
bartekzylinski <bartlomiej.zylinski@softwaremill.pl>
2021-09-27 13:56:44 +0200

    Refactor: replace Int with Long

--- core/src/main/scala/sttp/model/headers/Range.scala
+++ core/src/main/scala/sttp/model/headers/Range.scala
@@ -5,41 +5,41 @@ import sttp.model.{ContentRangeUnits, HeaderNames}
 
 import scala.util.{Failure, Success, Try}
 
-case class RangeHeader(start: Option[Int], end: Option[Int], unit: String) {
+case class Range(start: Option[Long], end: Option[Long], unit: String) {
 
-  val range: Range = Range(start, end)
+  val range: RangeValue = RangeValue(start, end)
 
-  override def toString: String = s"${HeaderNames.Range}: $unit=${start.getOrElse(0)}-${end.getOrElse(0)}"
+  override def toString: String = s"${HeaderNames.Range}: $unit=${start.getOrElse("")}-${end.getOrElse("")}"
 }
 
-object RangeHeader {
+object Range {
 
-  def parse(str: String): Either[String, List[RangeHeader]] =
+  def parse(str: String): Either[String, List[Range]] =
     Try(parseString(str))
     .filter(_.forall(isValid)) match {
       case Success(value) => Right(value)
       case Failure(exception) => Left(exception.getMessage)
     }
 
-  private def parseString(str: String): List[RangeHeader] = {
+  private def parseString(str: String): List[Range] = {
     val splited = str.split("=")
     val unit = splited(0)
     if (splited(1).contains(",")) splited(1).split(",").map(s => parsSingleRange(s, unit)).toList
     else List(parsSingleRange(splited(1), unit))
   }
 
-  private def parsSingleRange(rangeString: String, unit: String): RangeHeader = {
+  private def parsSingleRange(rangeString: String, unit: String): Range = {
     val strings = rangeString.trim.split("-")
-    if (strings.size == 2) RangeHeader(toIntOption(strings(0)), toIntOption(strings(1)), unit)
-    else RangeHeader(toIntOption(strings(0)), None, unit)
+    if (strings.size == 2) Range(toLongOption(strings(0)), toLongOption(strings(1)), unit)
+    else Range(toLongOption(strings(0)), None, unit)
   }
 
-  private def toIntOption(s: String): Option[Int] = {
-    if(s != null) None
-    else Some(s.toInt)
+  private def toLongOption(s: String): Option[Long] = {
+    if(s.isEmpty) None
+    else Some(s.toLong)
   }
 
-  private def isValid(range: RangeHeader): Boolean = {
+  private def isValid(range: Range): Boolean = {
     val start = range.start
     val end = range.end
     val isCorrectlyDefined = start.isDefined || end.isDefined
@@ -49,14 +49,14 @@ object RangeHeader {
     } else isCorrectlyDefined
   }
 
-  def unsafeApply(s: String): List[RangeHeader] = safeApply(s).getOrThrow
-  def safeApply(s: String): Either[String, List[RangeHeader]] = parse(s)
+  def unsafeApply(s: String): List[Range] = safeApply(s).getOrThrow
+  def safeApply(s: String): Either[String, List[Range]] = parse(s)
 }
 
-case class Range(start: Option[Int], end: Option[Int]) {
+case class RangeValue(start: Option[Long], end: Option[Long]) {
   def isValid(contentLength: Long): Boolean = if (end.exists(e => e < contentLength)) true else false
 
   def toContentRange(fileSize: Long, unit: String = ContentRangeUnits.Bytes): String = unit + " " + start.getOrElse(0) + "-" + end.getOrElse(0) + "/" + fileSize
 
-  val contentLength: Int = end.zip(start).map(r => r._1 - r._2).headOption.getOrElse(0)
+  val contentLength: Long = end.zip(start).map(r => r._1 - r._2).headOption.getOrElse(0)
 }
--- core/src/test/scala/sttp/model/headers/RangeHeaderTest.scala
+++ core/src/test/scala/sttp/model/headers/RangeHeaderTest.scala
@@ -7,79 +7,79 @@ import sttp.model.ContentRangeUnits
 class RangeHeaderTest extends AnyFlatSpec with Matchers {
 
   it should "properly pars simplest Range header" in {
-    val actual = RangeHeader.parse("bytes=200-1000")
-    actual shouldBe Right(List(RangeHeader(Some(200), Some(1000), ContentRangeUnits.Bytes)))
+    val actual = Range.parse("bytes=200-1000")
+    actual shouldBe Right(List(Range(Some(200), Some(1000), ContentRangeUnits.Bytes)))
   }
 
   it should "properly parse Range without star" in {
-    val actual = RangeHeader.parse("bytes=-1000")
-    actual shouldBe Right(List(RangeHeader(None, Some(1000), ContentRangeUnits.Bytes)))
+    val actual = Range.parse("bytes=-1000")
+    actual shouldBe Right(List(Range(None, Some(1000), ContentRangeUnits.Bytes)))
   }
 
   it should "properly parse Range without end" in {
-    val actual = RangeHeader.parse("bytes=200-")
-    actual shouldBe Right(List(RangeHeader(Some(200), None, ContentRangeUnits.Bytes)))
+    val actual = Range.parse("bytes=200-")
+    actual shouldBe Right(List(Range(Some(200), None, ContentRangeUnits.Bytes)))
   }
 
   it should "properly parse Range simple multirange" in {
-    val actual = RangeHeader.parse("bytes=200-1000, 1200-1400")
+    val actual = Range.parse("bytes=200-1000, 1200-1400")
     val expectedHeaders = List(
-      RangeHeader(Some(200), Some(1000), ContentRangeUnits.Bytes),
-      RangeHeader(Some(1200), Some(1400), ContentRangeUnits.Bytes))
+      Range(Some(200), Some(1000), ContentRangeUnits.Bytes),
+      Range(Some(1200), Some(1400), ContentRangeUnits.Bytes))
     actual shouldBe Right(expectedHeaders)
   }
 
   it should "properly parse Range mutltirange without end" in {
-    val actual = RangeHeader.parse("bytes=200-1000, 1200-")
+    val actual = Range.parse("bytes=200-1000, 1200-")
     val expectedHeaders = List(
-      RangeHeader(Some(200), Some(1000), ContentRangeUnits.Bytes),
-      RangeHeader(Some(1200), None, ContentRangeUnits.Bytes))
+      Range(Some(200), Some(1000), ContentRangeUnits.Bytes),
+      Range(Some(1200), None, ContentRangeUnits.Bytes))
     actual shouldBe Right(expectedHeaders)
   }
 
   it should "properly parse Range mutltirange without start" in {
-    val actual = RangeHeader.parse("bytes=200-1000, -1400")
+    val actual = Range.parse("bytes=200-1000, -1400")
     val expectedHeaders = List(
-      RangeHeader(Some(200), Some(1000), ContentRangeUnits.Bytes),
-      RangeHeader(None, Some(1400), ContentRangeUnits.Bytes))
+      Range(Some(200), Some(1000), ContentRangeUnits.Bytes),
+      Range(None, Some(1400), ContentRangeUnits.Bytes))
     actual shouldBe Right(expectedHeaders)
   }
 
   it should "properly parse Range mutltirange(3) without end" in {
-    val actual = RangeHeader.parse("bytes=400-1600, 1800-1900, 2100-")
+    val actual = Range.parse("bytes=400-1600, 1800-1900, 2100-")
     val expectedHeaders = List(
-      RangeHeader(Some(400), Some(1600), ContentRangeUnits.Bytes),
-      RangeHeader(Some(1800), Some(1900), ContentRangeUnits.Bytes),
-      RangeHeader(Some(2100), None, ContentRangeUnits.Bytes))
+      Range(Some(400), Some(1600), ContentRangeUnits.Bytes),
+      Range(Some(1800), Some(1900), ContentRangeUnits.Bytes),
+      Range(Some(2100), None, ContentRangeUnits.Bytes))
     actual shouldBe Right(expectedHeaders)
   }
 
   it should "properly parse Range mutltirange(3) without start" in {
-    val actual = RangeHeader.parse("bytes=500-700, 900-1000, -1300")
+    val actual = Range.parse("bytes=500-700, 900-1000, -1300")
     val expectedHeaders = List(
-      RangeHeader(Some(500), Some(700), ContentRangeUnits.Bytes),
-      RangeHeader(Some(900), Some(1000), ContentRangeUnits.Bytes),
-      RangeHeader(None, Some(1300), ContentRangeUnits.Bytes))
+      Range(Some(500), Some(700), ContentRangeUnits.Bytes),
+      Range(Some(900), Some(1000), ContentRangeUnits.Bytes),
+      Range(None, Some(1300), ContentRangeUnits.Bytes))
     actual shouldBe Right(expectedHeaders)
   }
 
   it should "fail parsing random string" in {
-    val actual = RangeHeader.parse("Opuncja")
+    val actual = Range.parse("Opuncja")
     actual shouldBe Left("Index 1 out of bounds for length 1")
   }
 
   it should "fail parsing header with incorrect range" in {
-    val actual = RangeHeader.parse("bytes=700-500")
+    val actual = Range.parse("bytes=700-500")
     actual shouldBe Left("Predicate does not hold for List(Range: bytes=700-500)")
   }
 
   it should "fail parsing header without correct range" in {
-    val actual = RangeHeader.parse("bytes=-")
+    val actual = Range.parse("bytes=-")
     actual shouldBe Left("Index 0 out of bounds for length 0")
   }
 
   it should "fail for partially correct multiheader" in {
-    val actual = RangeHeader.parse("bytes=500-700, 900-800")
+    val actual = Range.parse("bytes=500-700, 900-800")
     actual shouldBe Left("Predicate does not hold for List(Range: bytes=500-700, Range: bytes=900-800)")
   }
 }
