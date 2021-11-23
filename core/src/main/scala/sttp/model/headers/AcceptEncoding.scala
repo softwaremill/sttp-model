6d79b20bc2efb489da98ad36c82ee42a0d6d6853
Pask <pask423th@gmail.com>
2021-11-23 19:13:59 +0100

    Refactored parsing and tests

--- core/src/main/scala/sttp/model/headers/AcceptEncoding.scala
+++ core/src/main/scala/sttp/model/headers/AcceptEncoding.scala
@@ -2,52 +2,65 @@ package sttp.model.headers
 
 import sttp.model.internal.Validate.RichEither
 
-case class AcceptEncoding(compressionAlgorithm: String, weight: Option[BigDecimal]) {
+import scala.annotation.tailrec
 
-  override def toString: String = s"$compressionAlgorithm${weight.map(w => s";q=$w").getOrElse("")}"
+case class Encoding(encoding: String, weight: Option[BigDecimal]) {
+  override def toString: String = s"$encoding${weight.map(w => s";q=$w").getOrElse("")}"
+}
+
+case class AcceptEncoding(encodings: List[Encoding]) {
+
+  override def toString: String = encodings.map(_.toString).mkString(",")
 }
 
 object AcceptEncoding {
 
-  def parse(str: String): List[Either[String, AcceptEncoding]] =
-    str.trim
-      .split(",")
-      .toList
-      .map(processString)
+  def parse(str: String): Either[String, AcceptEncoding] = {
+    val encodings = processString(str, List.empty)
+    if (encodings.forall(isValid) && encodings.nonEmpty) Right(AcceptEncoding(encodings.reverse))
+    else if (encodings.isEmpty)
+      Left("Expected Accept-Encoding in the format: \"deflate or gzip;q=1.0\", but got: %s".format(str))
+    else Left("Invalid Encoding")
+  }
 
-  private def processString(s: String): Either[String, AcceptEncoding] = {
-    s.trim.split(";") match {
+  @tailrec
+  private def processString(str: String, acc: List[Encoding]): List[Encoding] = {
+    str.trim.split(",").toList match {
+      case x :: tail if x.nonEmpty =>
+        val range = parsSingleEncoding(x)
+        processString(tail.mkString(","), range :: acc)
+      case Nil => List(parsSingleEncoding(str))
+      case _   => acc
+    }
+  }
+
+  private def parsSingleEncoding(s: String): Encoding = {
+    s.split(";") match {
       case Array(algorithm, weight) =>
         weight.split("=") match {
-          case Array(_, value) =>
-            val encoding = AcceptEncoding(algorithm, Some(BigDecimal(value)))
-            if (isValid(encoding)) Right(encoding)
-            else Left("Invalid Encoding")
-          case _ =>  Left("Expected Accept-Encoding weight in the format: \"q=1.0\", but got: %s".format(weight))
+          case Array(_, value) => Encoding(algorithm, Some(BigDecimal(value)))
+          case _               => Encoding("", None)
         }
-      case Array(algorithm) =>
-        val encoding = AcceptEncoding(algorithm, None)
-        if (isValid(encoding)) Right(encoding)
-        else Left("Invalid Encoding")
-      case _ => Left("Expected Accept-Encoding in the format: \"deflate or gzip;q=1.0\", but got: %s".format(s))
+      case Array(algorithm) => Encoding(algorithm, None)
+      case _                => Encoding("", None)
     }
   }
 
-  private def isValid(acceptEncoding: AcceptEncoding): Boolean = {
+  private def isValid(acceptEncoding: Encoding): Boolean = {
     acceptEncoding.weight match {
-      case None        => acceptEncoding.compressionAlgorithm.nonEmpty
-      case Some(value) => (value <= 1 && value >= 0) && acceptEncoding.compressionAlgorithm.nonEmpty
+      case None        => acceptEncoding.encoding.nonEmpty
+      case Some(value) => (value <= 1 && value >= 0) && acceptEncoding.encoding.nonEmpty
     }
   }
 
-  def unsafeParse(s: String): List[AcceptEncoding] = parse(s).map(_.getOrThrow)
+  def unsafeParse(s: String): AcceptEncoding = parse(s).getOrThrow
 
-  def unsafeApply(compressionAlgorithm: String, weight: Option[BigDecimal]): AcceptEncoding =
-    safeApply(compressionAlgorithm, weight).getOrThrow
+  def unsafeApply(encoding: String, weight: Option[BigDecimal]): AcceptEncoding =
+    safeApply(encoding, weight).getOrThrow
 
-  def safeApply(compressionAlgorithm: String, weight: Option[BigDecimal]): Either[String, AcceptEncoding] = {
-    val encoding = AcceptEncoding(compressionAlgorithm, weight)
-    if (isValid(encoding)) Right(encoding)
+  def safeApply(encoding: String, weight: Option[BigDecimal]): Either[String, AcceptEncoding] = {
+    val encodingObject = Encoding(encoding, weight)
+    if (isValid(encodingObject)) Right(AcceptEncoding(List(encodingObject)))
     else Left("Invalid Accept-Encoding header")
   }
 }
--- core/src/test/scala/sttp/model/headers/AcceptEncodingTest.scala
+++ core/src/test/scala/sttp/model/headers/AcceptEncodingTest.scala
@@ -7,80 +7,88 @@ import sttp.model.Encodings
 class AcceptEncodingTest extends AnyFlatSpec with Matchers {
 
   it should "properly parse simplest Accept-Encoding header" in {
-    val actual = AcceptEncoding.parse("deflate").head
-    actual shouldBe Right(AcceptEncoding(Encodings.Deflate, None))
+    AcceptEncoding.parse("deflate") shouldBe Right(AcceptEncoding(List(Encoding(Encodings.Deflate, None))))
   }
 
   it should "properly parse Accept-Encoding with wildcard" in {
-    val actual = AcceptEncoding.parse("*").head
-    actual shouldBe Right(AcceptEncoding(Encodings.Wildcard, None))
+    AcceptEncoding.parse("*") shouldBe Right(AcceptEncoding(List(Encoding(Encodings.Wildcard, None))))
   }
 
   it should "properly parse Accept-Encoding header with multiple encodings" in {
-    val actual = AcceptEncoding.parse("gzip, deflate")
-    val gzip = Right(AcceptEncoding(Encodings.Gzip, None))
-    val deflate = Right(AcceptEncoding(Encodings.Deflate, None))
-    actual shouldBe List(gzip, deflate)
+    val encodings = List(Encoding(Encodings.Gzip, None), Encoding(Encodings.Deflate, None))
+    AcceptEncoding.parse("gzip, deflate") shouldBe Right(AcceptEncoding(encodings))
   }
 
   it should "properly parse Accept-Encoding header with multiple encodings and wildcard" in {
-    val actual = AcceptEncoding.parse("gzip, deflate, *")
-    val gzip = Right(AcceptEncoding(Encodings.Gzip, None))
-    val deflate = Right(AcceptEncoding(Encodings.Deflate, None))
-    val wildcard= Right(AcceptEncoding(Encodings.Wildcard, None))
-    actual shouldBe List(gzip, deflate, wildcard)
+    val encodings =
+      List(Encoding(Encodings.Gzip, None), Encoding(Encodings.Deflate, None), Encoding(Encodings.Wildcard, None))
+    AcceptEncoding.parse("gzip, deflate, *") shouldBe Right(AcceptEncoding(encodings))
   }
 
   it should "properly parse Accept-Encoding header with weight" in {
-    val actual = AcceptEncoding.parse("deflate;q=1.0").head
-    actual shouldBe Right(AcceptEncoding(Encodings.Deflate, Some(BigDecimal("1.0"))))
+    val actual = AcceptEncoding.parse("deflate;q=1.0")
+    actual shouldBe Right(AcceptEncoding(List(Encoding(Encodings.Deflate, Some(BigDecimal("1.0"))))))
   }
 
   it should "properly parse complex Accept-Encoding header" in {
-    val actual = AcceptEncoding.parse("deflate, gzip;q=0.75, *;q=0.5")
-    val deflate = Right(AcceptEncoding(Encodings.Deflate, None))
-    val gzip = Right(AcceptEncoding(Encodings.Gzip, Some(BigDecimal("0.75"))))
-    val wildcard= Right(AcceptEncoding(Encodings.Wildcard, Some(BigDecimal("0.5"))))
-    actual shouldBe List(deflate, gzip, wildcard)
+    val encodings = List(
+      Encoding(Encodings.Deflate, None),
+      Encoding(Encodings.Gzip, Some(BigDecimal("0.75"))),
+      Encoding(Encodings.Wildcard, Some(BigDecimal("0.5")))
+    )
+    AcceptEncoding.parse("deflate,gzip;q=0.75,*;q=0.5") shouldBe Right(AcceptEncoding(encodings))
+  }
+
+  it should "properly parse complex Accept-Encoding header with spaces" in {
+    val encodings = List(
+      Encoding(Encodings.Deflate, None),
+      Encoding(Encodings.Gzip, Some(BigDecimal("0.75"))),
+      Encoding(Encodings.Wildcard, Some(BigDecimal("0.5")))
+    )
+    AcceptEncoding.parse("deflate, gzip;q=0.75, *;q=0.5") shouldBe Right(AcceptEncoding(encodings))
   }
 
   it should "properly parse strange header" in {
-    val actual = AcceptEncoding.parse("deflate,")
-    actual shouldBe List(Right(AcceptEncoding(Encodings.Deflate, None)))
+    AcceptEncoding.parse("deflate,") shouldBe Right(AcceptEncoding(List(Encoding(Encodings.Deflate, None))))
   }
 
   it should "properly parse custom encoding" in {
-    val actual = AcceptEncoding.parse("tapirus").head
-    actual shouldBe Right(AcceptEncoding("tapirus", None))
+    AcceptEncoding.parse("tapirus") shouldBe Right(AcceptEncoding(List(Encoding("tapirus", None))))
   }
 
   it should "properly parse custom encoding with weight" in {
-    val actual = AcceptEncoding.parse("tapirus;q=0.1").head
-    actual shouldBe Right(AcceptEncoding("tapirus", Some(BigDecimal("0.1"))))
+    val encodings = List(Encoding("tapirus", Some(BigDecimal("0.1"))))
+    AcceptEncoding.parse("tapirus;q=0.1") shouldBe Right(AcceptEncoding(encodings))
   }
 
   it should "fail while validating header with q more then 1" in {
-    val actual = AcceptEncoding.parse("gzip;q=1.1").head
-    actual shouldBe Left("Invalid Encoding")
+    AcceptEncoding.parse("gzip;q=1.1") shouldBe Left("Invalid Encoding")
   }
 
   it should "fail while validating empty header" in {
-    val actual = AcceptEncoding.parse("").head
-    actual shouldBe Left("Invalid Encoding")
+    AcceptEncoding.parse("") shouldBe Left(
+      "Expected Accept-Encoding in the format: \"deflate or gzip;q=1.0\", but got: "
+    )
   }
 
   it should "fail while parsing header with incorrect q" in {
-    val actual = AcceptEncoding.parse("gzip;q1.1").head
-    actual shouldBe Left("Expected Accept-Encoding weight in the format: \"q=1.0\", but got: q1.1")
+    AcceptEncoding.parse("gzip;q1.1") shouldBe Left("Invalid Encoding")
   }
 
   it should "fail while parsing incorrect header" in {
-    val actual = AcceptEncoding.parse(";").head
-    actual shouldBe Left("Expected Accept-Encoding in the format: \"deflate or gzip;q=1.0\", but got: ;")
+    AcceptEncoding.parse(";") shouldBe Left("Invalid Encoding")
   }
 
   it should "fail while validating header without algorithm" in {
-    val actual = AcceptEncoding.parse(";q=1.0").head
-    actual shouldBe Left("Invalid Encoding")
+    AcceptEncoding.parse(";q=1.0") shouldBe Left("Invalid Encoding")
+  }
+
+  it should "display correct header string" in {
+    val encodings = List(
+      Encoding(Encodings.Deflate, None),
+      Encoding(Encodings.Gzip, Some(BigDecimal("0.75"))),
+      Encoding(Encodings.Wildcard, Some(BigDecimal("0.5")))
+    )
+    AcceptEncoding(encodings).toString shouldBe "deflate,gzip;q=0.75,*;q=0.5"
   }
 }
