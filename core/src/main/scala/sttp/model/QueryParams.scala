8c35904f099a9f8eb25f2a2abd873e0e3389b090
adamw <adam@warski.org>
2021-12-21 16:42:45 +0100

    Add an option to serialise query params as a string with the ? boundary

--- core/src/main/scala/sttp/model/QueryParams.scala
+++ core/src/main/scala/sttp/model/QueryParams.scala
@@ -16,20 +16,45 @@ case class QueryParams(ps: Seq[(String, Seq[String])]) {
   def param(k: String, v: Seq[String]): QueryParams = new QueryParams(ps :+ (k -> v))
   def param(p: Map[String, String]): QueryParams = new QueryParams(ps ++ p.map { case (k, v) => (k -> List(v)) })
 
-  override def toString: String = toString(Uri.QuerySegmentEncoding.Standard, Uri.QuerySegmentEncoding.StandardValue)
-  def toString(keyEncoding: Uri.Encoding, valueEncoding: Uri.Encoding): String = {
-    ps.flatMap {
-      case (k, vs) if vs.isEmpty => List(keyEncoding(k))
-      case (k, vs)               => vs.map(v => s"${keyEncoding(k)}=${valueEncoding(v)}")
-    }.mkString("&")
+  override def toString: String = toString(includeBoundary = false)
+  def toString(includeBoundary: Boolean): String =
+    toString(Uri.QuerySegmentEncoding.Standard, Uri.QuerySegmentEncoding.StandardValue, includeBoundary)
+  def toString(keyEncoding: Uri.Encoding, valueEncoding: Uri.Encoding): String =
+    toString(keyEncoding, valueEncoding, includeBoundary = false)
+
+  /** @param includeBoundary
+    *   Should the boundary `?` character be added preceding the result, if the query params are non-empty.
+    */
+  def toString(keyEncoding: Uri.Encoding, valueEncoding: Uri.Encoding, includeBoundary: Boolean): String = {
+    val boundary = if (includeBoundary && ps.nonEmpty) "?" else ""
+    boundary + ps
+      .flatMap {
+        case (k, vs) if vs.isEmpty => List(keyEncoding(k))
+        case (k, vs)               => vs.map(v => s"${keyEncoding(k)}=${valueEncoding(v)}")
+      }
+      .mkString("&")
   }
 }
 
 object QueryParams {
   def apply() = new QueryParams(Nil)
   def fromMap(m: Map[String, String]): QueryParams = new QueryParams(m.mapValues(List(_)).toSeq)
-  def fromSeq(s: Seq[(String, String)]): QueryParams =
-    new QueryParams(s.groupBy(_._1).mapValues(_.map(_._2)).toSeq)
+  def fromSeq(s: Seq[(String, String)]): QueryParams = {
+    // Building the sequence of parameters so that their relative order is kept. O(n^2), but there shouldn't be too many query parameters.
+    def add(acc: List[(String, List[String])], kv: (String, String)): List[(String, List[String])] = {
+      val (k, v) = kv
+      def go(current: List[(String, List[String])]): List[(String, List[String])] = current match {
+        case Nil                          => (k -> (v :: Nil)) :: Nil
+        case head :: tail if head._1 == k => (head._1, head._2 :+ v) :: tail
+        case head :: tail                 => head :: go(tail)
+      }
+
+      go(acc)
+    }
+
+    new QueryParams(s.foldLeft(Nil: List[(String, List[String])])(add))
+  }
+
   def fromMultiMap(m: Map[String, Seq[String]]): QueryParams = new QueryParams(m.toSeq)
   def fromMultiSeq(m: Seq[(String, Seq[String])]): QueryParams = new QueryParams(m)
 }
