475d94298f323906371cb31ba7c495fda19690d8
bartlomiej.zylinski <bartlomiej.zylinski@softwaremill.pl>
2022-11-13 18:51:01 +0100

    Added HTTP_3

--- core/src/main/scala/sttp/model/HttpVersion.scala
+++ core/src/main/scala/sttp/model/HttpVersion.scala
@@ -2,8 +2,8 @@ package sttp.model
 
 sealed trait HttpVersion
 object HttpVersion {
-  case object Default extends HttpVersion
   case object HTTP_1 extends HttpVersion
   case object HTTP_1_1 extends HttpVersion
   case object HTTP_2 extends HttpVersion
+  case object HTTP_3 extends HttpVersion
 }
