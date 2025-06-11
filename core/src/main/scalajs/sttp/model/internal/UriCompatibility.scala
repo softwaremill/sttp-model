ccf770702242f24e7d1a0b911025acff5ce2f5d5
adamw <adam@warski.org>
2025-06-11 22:28:51 +0200

    Revert change

--- core/src/main/scalajs/sttp/model/internal/UriCompatibility.scala
+++ core/src/main/scalajs/sttp/model/internal/UriCompatibility.scala
@@ -7,8 +7,7 @@ import scala.scalajs.js.URIUtils
 private[sttp] object UriCompatibility {
   def encodeDNSHost(s: String): String = {
     // the URL class IDN formats and escapes the host
-    // new URL(s"http://$s").host
-    new URL(s"http://${URIUtils.encodeURIComponent(s)}").host
+    new URL(s"http://$s").host
   }
 
   def encodeQuery(s: String, enc: String): String = URIUtils.encodeURIComponent(s)
