d3834a738d8e160a8f484f59c4c01f0441eb9001
Dilyan Georgiev <digeorgiev@mdsol.com>
2025-07-02 11:35:23 +0100

    Add support for filename* on disposition header getter

--- core/src/test/scala/sttp/model/PartTest.scala
+++ core/src/test/scala/sttp/model/PartTest.scala
@@ -14,6 +14,19 @@ class PartTest extends AnyFlatSpec with Matchers {
     part.contentDispositionHeaderValue shouldBe """form-data; name="name"; filename="fó1.txt""""
   }
 
+  it should "retain non-ascii characters in filename*" in {
+    val expectedFileName = "fó1.txt"
+    val part = Part("name", 42, otherDispositionParams = Map("filename*" -> expectedFileName)).fileName("f1.txt")
+    part.contentDispositionHeaderValue shouldBe s"""form-data; name="name"; filename*="$expectedFileName"; filename="f1.txt""""
+    part.fileName shouldBe Some(expectedFileName)
+  }
+
+  it should "fallback to basic filename if filename* is not present" in {
+    val part = Part("name", 42).fileName("f1.txt")
+    part.contentDispositionHeaderValue shouldBe s"""form-data; name="name"; filename="f1.txt""""
+    part.fileName shouldBe Some("f1.txt")
+  }
+
   it should "escape double quote and backslash in filename" in {
     val part = Part("name", 42).fileName("f\"\\1.txt")
     part.contentDispositionHeaderValue shouldBe """form-data; name="name"; filename="f\"\\1.txt""""
